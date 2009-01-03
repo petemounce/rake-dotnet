a74e16da529dcb27959459d0a66174cdae679da1
Peter Mounce <pete@neverrunwithscissors.com>
2009-01-03 10:13:15 +0000

    Allow to pass in variables from the command-line to specify product- and company-name, and configuration to build in - and call msbuild against the solution file (which presently, will build everything in it, all or nothing.)

--- rake_dotnet/test-solution/Rakefile.rb
+++ rake_dotnet/test-solution/Rakefile.rb
@@ -7,11 +7,22 @@ require 'rake/clean'
 require '../rake_dotnet'
 require '../assemblyinfo'
 require '../file'
+require '../msbuild'
 require '../svn'
 require '../version'
 
+PRODUCT = ENV['PRODUCT'] ? ENV['PRODUCT'] : 'Yoti'
+COMPANY = ENV['COMPANY'] ? ENV['COMPANY'] : 'YotiCo'
+CONFIGURATION = ENV['CONFIGURATION'] ? ENV['CONFIGURATION'] : 'Debug'
 
+# Source files
+srcDir = 'src'
+
+# Generated files
 buildDir = 'build'
+versionTxt = File.join(buildDir,'version.txt')
+asmInfoCs = File.join(srcDir,'AssemblyInfo.cs')
+projects = 'solution_projects.txt'
 
 # clean will remove intermediate files (like the output of msbuild; things in the src tree)
 # clobber will remove build-output files (which will all live under the build tree)
@@ -19,11 +30,23 @@ CLEAN.exclude('**/core') # core files are a Ruby/*nix thing - dotNET developers 
 CLEAN.include('src/**/obj')
 CLEAN.include('src/**/bin')
 CLEAN.include('src/**/AssemblyInfo.cs')
+CLEAN.include('solution_projects.txt')
 CLOBBER.include(buildDir)
 
-directory buildDir
-versionTxt = File.join('build','version.txt')
-asmInfoCs = File.join('src','AssemblyInfo.cs')
 
-vt = Rake::VersionTask.new(versionTxt)
-ai = Rake::AssemblyInfoTask.new(asmInfoCs, versionTxt)
+directory buildDir
+
+Rake::VersionTask.new(versionTxt)
+Rake::AssemblyInfoTask.new(asmInfoCs, versionTxt) do |ai|
+	# TODO: Read {configuration, product, company} from Rakefile.yaml config file ?
+	ai.product_name = PRODUCT
+	ai.company_name = COMPANY
+	ai.configuration = CONFIGURATION
+end
+
+
+task :debug => [:version, :assembly_info] do |t|
+	src = Pathname.new('src')
+	mb = MsBuild.new("#{PRODUCT}.sln", {:Configuration => CONFIGURATION}, ['Build'])
+	mb.run
+end
