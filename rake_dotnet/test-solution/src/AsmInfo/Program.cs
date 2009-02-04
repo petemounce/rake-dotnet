c8f5914bd98df276c3c70e5547f1b5a859973ec7
Peter Mounce <pete@neverrunwithscissors.com>
2009-02-04 10:49:45 +0000

    Hack together a little application to read the version out of a DLL and echo it to the console

---
+++ rake_dotnet/test-solution/src/AsmInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AsmInfo
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			if (args.Length != 1 || string.IsNullOrEmpty(args[0]))
+			{
+				Console.WriteLine(@"Usage: AsmInfo {relative-path-to-dll}");
+				return;
+			}
+			var asmRelPath = args[0].Trim();
+			var asmAbsPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + asmRelPath;
+			var asmFile = Assembly.LoadFile(asmAbsPath);
+			Console.WriteLine(asmFile.GetName().Version.ToString(4));
+		}
+	}
+}
