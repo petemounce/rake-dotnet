a2d8bd576e1a2224d61bc60e5bb0fc4b09cc9d76
Peter Mounce <pete@neverrunwithscissors.com>
2009-03-26 11:02:09 +0000

    Cosmetic

--- rake_dotnet/lib/assemblyinfo.rb
+++ rake_dotnet/lib/assemblyinfo.rb
@@ -19,10 +19,7 @@ module Rake
 				end
 				of = Pathname.new(@name)
 				of.delete if of.exist?
-				#File.write(of, content)
-				of.open('w') {|f|
-					f.puts content
-				}
+				of.open('w') { |f| f.puts content }
 			end
 			
 			desc 'Generate the AssemblyInfo.cs file from the template'
