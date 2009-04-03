624bfaf53123b7bf3c85132a47efee2a8e29acbe
Peter Mounce <pete@neverrunwithscissors.com>
2009-04-03 10:09:56 +0000

    Support git, by not assuming Subversion.  Instead of writing in an svn revision number, therefore, write in today's date as YYYYMMDD.

--- rake_dotnet/lib/version.rb
+++ rake_dotnet/lib/version.rb
@@ -1,15 +1,27 @@
 class Versioner
-	def initialize(template_file=nil)
-		tf = template_file || 'version.template.txt'
-		template_file = Pathname.new(tf)
-		@maj_min = template_file.read.chomp
-		@build = ENV['BUILD_NUMBER'] || 0
-		@svn_info = SvnInfo.new(:path => '.')
-	end
-	
-	def get
-		"#{@maj_min}.#{@build}.#{@svn_info.revision}"
-	end
+  def initialize(template_file=nil)
+    tf = template_file || 'version.template.txt'
+    file = Pathname.new(tf)
+    @maj_min = file.read.chomp
+  end
+
+  def get
+    "#{@maj_min}.#{build}.#{revision}"
+  end
+
+  def build
+    ENV['BUILD_NUMBER'] || 0
+  end
+
+  def revision
+    if (Pathname.new('.svn').exist?)
+      puts foo
+      SvnInfo.new(:path => '.').revision
+    else
+      now = Date.today
+      "#{now.to_s.gsub('-','')}"
+    end
+  end
 end
 
 
