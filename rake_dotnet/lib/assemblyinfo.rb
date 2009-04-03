423fec40701823e2c11fefe4817f0d1d96f645b9
Peter Mounce <pete@neverrunwithscissors.com>
2009-04-03 11:18:02 +0000

    Generate the AssemblyInfo.cs files in place, and allow for overriding them with a template file next-door to them.  This reduces friction (before, had to individually link files when a new project was created).

--- rake_dotnet/lib/assemblyinfo.rb
+++ rake_dotnet/lib/assemblyinfo.rb
@@ -1,61 +1,85 @@
 module Rake
-	class AssemblyInfoTask < Rake::TaskLib
-		attr_accessor :name, :product_name, :configuration, :company_name, :version
+  class AssemblyInfoTask < Rake::TaskLib
+    attr_accessor :product_name, :configuration, :company_name, :version
 
-		# Create an AssemblyInfo file-task; define a task to run it whose name is :assembly_info
-		def initialize(name) # :yield: self
-			@name = name
-			yield self if block_given?
-			define
-		end
+    def initialize(params={})
+      @src_dir = params[:src_dir] || SRC_DIR
+      yield self if block_given?
+      define
+    end
 
-		# Create the tasks defined by this task lib.
-		def define
-			file @name do
-				template_file = Pathname.new(template)
-				content = template_file.read
-				token_replacements.each do |key,value|
-					content = content.gsub(/(\$\{#{key}\})/, value.to_s)
-				end
-				of = Pathname.new(@name)
-				of.delete if of.exist?
-				of.open('w') { |f| f.puts content }
-			end
-			
-			desc 'Generate the AssemblyInfo.cs file from the template'
-			task :assembly_info => [@name]
+    def define
+      src_dir_regex = regexify(@src_dir)
+      rule(/#{src_dir_regex}\/[\w\.\d]+\/Properties\/AssemblyInfo.cs/) do |r|
+        nextdoor = Pathname.new(r.name + '.template')
+        common = Pathname.new(File.join(@src_dir, 'AssemblyInfo.cs.template'))
+        if (nextdoor.exist?)
+          generate(nextdoor, r.name)
+        elsif (common.exist?)
+          generate(common, r.name)
+        end
+      end
 
-			self
-		end
-		
-		def template
-			@template_file ||= @name.sub(/\.cs/, '.template.cs')
-		end
-		
-		def token_replacements
-			r = {}
-			r[:built_on] = Time.now
-			r[:product] = product_name
-			r[:configuration] = configuration
-			r[:company] = company_name
-			r[:version] = version
-			return r
-		end
-		
-		def product_name
-			@product_name ||= 'NRWS rake_dotnet'
-		end
-		
-		def configuration
-			@configuration ||= 'Debug'
-		end
-		
-		def company_name
-			@company_name ||= 'NRWS'
-		end
-		
-		def version
-			@version ||= Versioner.new.get
-		end
-	end
+      desc 'Generate the AssemblyInfo.cs file from the template closest'
+      task :assembly_info do |t|
+        # for each project, invoke the rule
+        Dir.foreach(@src_dir) do |e|
+          asm_info = File.join(@src_dir, e, 'Properties', 'AssemblyInfo.cs')
+          if is_project e
+            Rake::FileTask[asm_info].invoke
+          end
+        end
+      end
+
+      self
+    end
+
+    def generate(template_file, destination)
+      content = template_file.read
+      token_replacements.each do |key, value|
+        content = content.gsub(/(\$\{#{key}\})/, value.to_s)
+      end
+      of = Pathname.new(destination)
+      of.delete if of.exist?
+      of.open('w') { |f| f.puts content }
+    end
+
+    def is_project entry
+      if (entry == '.' || entry == '..' || entry == '.svn')
+        return false
+      end
+      if (entry == 'AssemblyInfo.cs.template')
+        return false
+      end
+      #puts "#{entry} is directory? #{File.directory?(entry)}"
+      #return File.directory?(entry)
+      return true
+    end
+
+    def token_replacements
+      r = {}
+      r[:built_on] = Time.now
+      r[:product] = product_name
+      r[:configuration] = configuration
+      r[:company] = company_name
+      r[:version] = version
+      return r
+    end
+
+    def product_name
+      @product_name ||= 'NRWS rake_dotnet'
+    end
+
+    def configuration
+      @configuration ||= 'Debug'
+    end
+
+    def company_name
+      @company_name ||= 'NRWS'
+    end
+
+    def version
+      @version ||= Versioner.new.get
+    end
+  end
 end
