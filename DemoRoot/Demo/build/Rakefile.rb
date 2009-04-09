430787b6b7df5ab6aafa64e4c6e193392e09a304
Peter Mounce <pete@neverrunwithscissors.com>
2009-04-09 15:45:41 +0100

    Add some for-convenience stuff, and will later figure out where it should live in rake-dotnet proper.

--- DemoRoot/Demo/build/Rakefile.rb
+++ DemoRoot/Demo/build/Rakefile.rb
@@ -35,3 +35,45 @@ Rake::RDNPackageTask.new(name='Demo.Site', version=RDNVERSION, {:deps=>[:harvest
 end
 
 task :default => [:compile, :harvest_output, :xunit, :package]
+
+
+# below here is stuff that will be refactored into rake-dotnet, but isn't yet...
+
+desc "Our script uploads artifacts that have a version/configuration in their name; sort it out for the runners after the initial build"
+task :ci_kludge do
+	# yes, I know this is a disgustingly brittle way to do it.
+	fl = FileList.new('out/bin*')
+	fl.each do |f|
+		pn = Pathname.new(f)
+		pn.rename 'out/bin'
+	end
+end
+
+CLEAN.include("#{SRC_DIR}/**/web.config")
+CLEAN.include("#{SRC_DIR}/**/app.config")
+
+task :configs,[:base_dirs, :environment] do |t, args|
+	args.with_defaults(:environment => 'dev')
+	all_base_dirs = FileList.new("#{SRC_DIR}/*")
+	args.with_defaults(:base_dirs => all_base_dirs)
+	args.base_dirs.each do |dir|
+		if dir == '.' || dir == '..' || dir == "../#{SRC_DIR}/AssemblyInfo.cs.template"
+			skip
+		end
+		if dir.match(/Site/)
+			pull_config(dir, 'web', args.environment)
+		else
+			pull_config(dir, 'app', args.environment)
+		end
+	end
+end
+
+def pull_config(dir, type, env)
+	template_config = File.join(dir, 'configs', env, "#{type}.template.config")
+	dest_config = File.join(dir, "#{type}.config")
+	if File.exist?(template_config)
+		cp template_config, dest_config 
+	end
+end
+
+task :first_checkout => [:configs, :assembly_info]
