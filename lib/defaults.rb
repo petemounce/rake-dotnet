258a5b96fe2a26c619102d38a264bb9b21de7483
Peter Mounce <pete@neverrunwithscissors.com>
2009-04-29 16:42:47 +0100

    Extraneous space

--- lib/defaults.rb
+++ lib/defaults.rb
@@ -22,7 +22,7 @@ def find_tools_dir
 	shared = File.join(PRODUCT_ROOT, '..', '3rdparty')
 	owned = File.join(PRODUCT_ROOT, '3rdparty')
 	if File.exist?(shared) 
-		return shared 
+		return shared
 	end
 	if File.exist?(owned) 
 		return owned
