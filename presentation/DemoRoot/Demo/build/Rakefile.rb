977970612d7ab0f9c9b99d9de70ab34db293d8a4
Peter Mounce <pete@neverrunwithscissors.com>
2009-06-15 23:13:03 +0100

    Readying for presentation tomorrow at Skillsmatter.

--- presentation/DemoRoot/Demo/build/Rakefile.rb
+++ presentation/DemoRoot/Demo/build/Rakefile.rb
@@ -34,7 +34,6 @@ task :default => [:compile, :harvest_output, :xunit, :package]
 
 
 # below here is stuff that will be refactored into rake-dotnet, but isn't yet...
-
 desc "Our script uploads artifacts that have a version/configuration in their name; sort it out for the runners after the initial build"
 task :ci_kludge do
 	# yes, I know this is a disgustingly brittle way to do it.
@@ -45,31 +44,4 @@ task :ci_kludge do
 	end
 end
 
-CLEAN.include("#{SRC_DIR}/**/web.config")
-CLEAN.include("#{SRC_DIR}/**/app.config")
-
-task :configs,[:base_dirs, :environment] do |t, args|
-	args.with_defaults(:environment => 'dev')
-	all_base_dirs = FileList.new("#{SRC_DIR}/*")
-	args.with_defaults(:base_dirs => all_base_dirs)
-	args.base_dirs.each do |dir|
-		if dir == '.' || dir == '..' || dir == "../#{SRC_DIR}/AssemblyInfo.cs.template"
-			skip
-		end
-		if dir.match(/Site/)
-			pull_config(dir, 'web', args.environment)
-		else
-			pull_config(dir, 'app', args.environment)
-		end
-	end
-end
-
-def pull_config(dir, type, env)
-	template_config = File.join(dir, 'configs', env, "#{type}.template.config")
-	dest_config = File.join(dir, "#{type}.config")
-	if File.exist?(template_config)
-		cp template_config, dest_config 
-	end
-end
-
-task :first_checkout => [:configs, :assembly_info]
+task :first_checkout => [:clobber, :assembly_info]
