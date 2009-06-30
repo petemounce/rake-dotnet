1c671827e25ab3fdd66fb50d53dfb3b37c741fb5
Peter Mounce <pete@neverrunwithscissors.com>
2009-06-30 14:20:23 +0100

    Attempting a fix for the TeamCity-multiple-configs situation I have, which unfortunately requires that I checkin code I'm not sure about...

--- lib/version.rb
+++ lib/version.rb
@@ -1,8 +1,9 @@
 class Versioner
-  def initialize(template_file=nil)
+  def initialize(template_file=nil, opts={})
     tf = template_file || 'version.template.txt'
     file = Pathname.new(tf)
     @maj_min = file.read.chomp
+	@bin_out = opts[:bin_out] || File.join(OUT_DIR, 'bin')
   end
 
   def get
@@ -10,7 +11,19 @@ class Versioner
   end
 
   def build
-	bn = ENV['BUILD_NUMBER']
+	fl = FileList.new("#{bin_out}*")
+	bn = 0
+	if ENV['TEAMCITY_BUILDCONF_NAME']
+		# use a previous binaries-build if one exists
+		fl.each do |e|
+			if (e.test(regexify(@bin_out)))
+				bn = e.match(/\w+-v\d+\.\d+\.(\d+)\.\d+\//)[1]
+				return bn
+			end
+		end
+		# otherwise use the current environment variable
+		bn = ENV['BUILD_NUMBER']
+	end
 	return 0 if bn == nil || !bn.match(/\d+/)
 	return bn
   end
