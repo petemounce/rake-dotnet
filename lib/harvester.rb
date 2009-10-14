b83f48c64ea5967c9c929ac36d20e0d774f3796d
Peter Mounce <pete@neverrunwithscissors.com>
2009-10-14 22:07:14 +0100

    NIY Specs for harvestoutputtask

--- lib/harvester.rb
+++ lib/harvester.rb
@@ -100,32 +100,32 @@ class HarvestWebApplicationTask < Rake::TaskLib
 		end
 	end
 end
-
-class Harvester
-	attr_accessor :files, :target
-
-	def initialize
-		@files = Hash.new
-	end
-
-	def add(glob)
-		toAdd = Dir.glob(glob)
-		toAdd.each do |a|
-			pn = Pathname.new(a)
-			@files[pn.basename.to_s] = pn
-		end
-	end
-
-	def harvest(target)
-		mkdir_p(target) unless File.exist?(target)
-		@files.sort.each do |k, v|
-			cp(v, target)
-		end
-	end
-
-	def list
-		@files.sort.each do |k, v|
-			puts k + ' -> ' + v
-		end
-	end
-end
+#
+#class Harvester
+#	attr_accessor :files, :target
+#
+#	def initialize
+#		@files = Hash.new
+#	end
+#
+#	def add(glob)
+#		toAdd = Dir.glob(glob)
+#		toAdd.each do |a|
+#			pn = Pathname.new(a)
+#			@files[pn.basename.to_s] = pn
+#		end
+#	end
+#
+#	def harvest(target)
+#		mkdir_p(target) unless File.exist?(target)
+#		@files.sort.each do |k, v|
+#			cp(v, target)
+#		end
+#	end
+#
+#	def list
+#		@files.sort.each do |k, v|
+#			puts k + ' -> ' + v
+#		end
+#	end
+#end
--- spec/harvestoutputtask_spec.rb
+++ spec/harvestoutputtask_spec.rb
@@ -2,5 +2,8 @@ require 'spec'
 require File.join(File.dirname(__FILE__), '..','lib','harvester.rb')
 
 describe HarvestOutputTask do
-
+	it "should use sensible defaults when initialising"
+	it "should be possible to set each parameter when initialising"
+	it "should define a directory task for @target_path"
+	it "should define :harvest_output depending on @target_path"
 end
