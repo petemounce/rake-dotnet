d77619043de6170c17754155fe0c416f0ed3d3b6
Peter Mounce <pete@neverrunwithscissors.com>
2010-01-05 13:44:18 +0000

    Remove unnecessary before/after steps

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,13 +4,6 @@ require 'fileutils'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'cli.rb')
 
 describe Cli do
-	before(:all) do
-		@here = File.dirname(__FILE__)
-		Dir.chdir('spec')
-	end
-	after :all do
-		Dir.chdir('..')
-	end
 	it "should push nil onto the array of search paths" do
 		cli = Cli.new
 		cli.search_paths.should == [nil]
@@ -28,19 +21,16 @@ describe Cli do
 		cli.cmd.should == '"foo"'
 	end
 	it "should return fully qualified path to exe when search-path is tried and exe is found there successfully" do
-		cli = Cli.new({:exe_name => 'foo.exe', :search_paths=>['support/cli']})
-		cli.cmd.should match(/#{@here}.*/)
-		cli.cmd.should include('support/cli/foo.exe')
+		cli = Cli.new({:exe_name => 'foo.exe', :search_paths=>['spec/support/cli']})
+		cli.cmd.should include('spec/support/cli/foo.exe')
 	end
 	it "should return fully qualified path to exe at first find when search-path is tried and exe is found" do
-		cli = Cli.new({:exe_name => 'foo.exe', :search_paths=>['support/cli/bar', 'support/cli']})
-		cli.cmd.should match(/#{@here}.*/)
-		cli.cmd.should include('support/cli/bar/foo.exe')
+		cli = Cli.new({:exe_name => 'foo.exe', :search_paths=>['spec/support/cli/bar', 'spec/support/cli']})
+		cli.cmd.should include('spec/support/cli/bar/foo.exe')
 	end
 	it "should return fq path to exe at first find, skipping non-finds" do
-		cli = Cli.new({:exe_name => 'foo.exe', :search_paths=>['support/cli/notexist', 'support/cli']})
-		cli.cmd.should match(/#{@here}.*/)
-		cli.cmd.should include('support/cli/foo.exe')
+		cli = Cli.new({:exe_name => 'foo.exe', :search_paths=>['spec/support/cli/notexist', 'spec/support/cli']})
+		cli.cmd.should include('spec/support/cli/foo.exe')
 	end
 	it "should throw if no exe found in search paths supplied and not in path" do
 		cli = Cli.new({:exe_name => 'nonexistent'})
