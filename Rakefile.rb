303e558968b8eceb66e478a042e6192ea531f92e
Peter Mounce <pete@neverrunwithscissors.com>
2010-01-07 11:01:47 +0000

    Reformat for readability

--- Rakefile.rb
+++ Rakefile.rb
@@ -25,7 +25,12 @@ end
 generated_library = File.join('lib', 'rake_dotnet.rb')
 CLOBBER.include generated_library
 file generated_library do |f|
-	files = ['header.rb', 'defaults.rb', 'cli.rb', 'bcpcmd.rb', 'sqlcmd.rb', 'assemblyinfo.rb', 'fxcop.rb', 'harvester.rb', 'msbuild.rb', 'ncover.rb', 'nunitcmd.rb', 'nunittask.rb', 'package.rb', 'sevenzip.rb', 'svn.rb', 'version.rb', 'xunit.rb', 'footer.rb']
+	files = ['header.rb', 'defaults.rb', 'cli.rb',
+             'bcpcmd.rb', 'sqlcmd.rb', 'assemblyinfo.rb',
+             'fxcop.rb', 'harvester.rb', 'msbuild.rb',
+             'ncover.rb', 'nunitcmd.rb', 'nunittask.rb',
+             'package.rb', 'sevenzip.rb', 'svn.rb',
+             'version.rb', 'xunit.rb', 'footer.rb']
 	gl = File.open(generated_library, 'a')
 	files.each do |file|
 		text = File.read(File.join('lib', file))
