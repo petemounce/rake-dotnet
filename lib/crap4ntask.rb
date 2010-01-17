86ebcfbdaa68bc250f86e9a0bb6cdc2d427f855f
Peter Mounce <pete@neverrunwithscissors.com>
2010-01-17 17:55:22 +0000

    Crap4nTask in progress.

--- lib/crap4ntask.rb
+++ lib/crap4ntask.rb
@@ -1,8 +1,10 @@
 class Crap4nTask < Rake::TaskLib
-	attr_accessor :out_dir
+	attr_accessor :out_dir, :coverage_dir, :metrics_dir
 
 	def initialize(params={})
 		@out_dir = params[:out_dir] || File.join(OUT_DIR, 'reports', 'crap4n')
+		@coverage_dir = params[:coverage_dir] || File.join(OUT_DIR, 'reports', 'ncover')
+		@metrics_dir = params[:metrics_dir] || File.join(OUT_DIR, 'reports', 'ncover')
 
 		yield self if block_given?
 		define
@@ -12,5 +14,19 @@ class Crap4nTask < Rake::TaskLib
 		directory @out_dir
 		task :crap4n => @out_dir
 		task :analyse => :crap4n
+
+		our_dir_regex = regexify(@out_dir)
+		rule(/#{out_dir_regex}\/.*\.crap4n\.xml/) do |r|
+
+		end
+
+		task :crap4n do
+			coverage_files = FileList.new("#{@coverage_dir}/*.coverage.xml")
+			metrics_files = FileList.new("#{@metrics_dir}/*.coverage.xml")
+			coverage_files.each do |cov|
+				name = Pathname.new(cov).basename.sub('.coverage.xml', '')
+				Rake::Task["#{@out_dir}/#{name}.crap4n.xml"]
+			end
+		end
 	end
 end
--- spec/crap4ntask_spec.rb
+++ spec/crap4ntask_spec.rb
@@ -35,11 +35,32 @@ describe Crap4nTask, 'When initialised with default settings' do
 	it 'should make :analyse depend on :crap4n' do
 		@analyse.prerequisites.should include('crap4n')
 	end
+	it 'should look in a sensible place for code-coverage data' do
+		@c4n.coverage_dir.should eql(File.join(OUT_DIR, 'reports', 'ncover'))
+	end
+	it 'should look in a sensible place for code-metrics data' do
+		@c4n.metrics_dir.should eql(File.join(OUT_DIR, 'reports', 'ncover'))
+	end
+	it 'should define a rule to invoke crap ( :-) ) when crap4n is run'
 end
 
 describe Crap4nTask, 'When initialised with an out_dir' do
 	it 'should use it' do
 		c4n = Crap4nTask.new(:out_dir=>'foo')
 		c4n.out_dir.should eql('foo')
+	end
+end
+
+describe Crap4nTask, 'When initialised with a coverage dir' do
+	it 'should use it' do
+		c4n = Crap4nTask.new(:coverage_dir=>'foo')
+		c4n.coverage_dir.should eql('foo')
+	end
+end
+
+describe Crap4nTask, 'When initialised with a metrics dir' do
+	it 'should use it' do
+		c4n = Crap4nTask.new(:metrics_dir=>'foo')
+		c4n.metrics_dir.should eql('foo')
 	end
 end
